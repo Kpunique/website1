f3e96de86e2451d1ab19d020084e1d4f65b0a546
Fred Scott <fscott@southeast.edu>
2017-07-25 10:14:39 -0500

    Adding index starter

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+require_once('database.php');
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>CHANGE ME</title>
+    </head>
+    <body>
+			<h1>Hello</h1>
+    </body>
+</html>
