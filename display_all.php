d9c58e6ff44e7a1188d718db554772036d31b0e5
tj598023 <tj598023@LR00902.southeast.edu>
2019-04-16 20:56:00 -0500

    Changed to fortune cookie App.  Missed this due to add* dd

---
+++ display_all.php
@@ -0,0 +1,30 @@
+<?php
+        require('database.php');
+        ?>
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <h2>ALL USER PAGE</h2>
+        
+        <table>
+            <tr>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>User Name</th>
+                <th>Email</th>
+                
+                <?php foreach ()
+            </tr>
+            
+        </table>
+    </body>
+</html>
--- index.php
+++ index.php
@@ -3,14 +3,56 @@ require_once('database.php');
 
 ?>
 
-
+<?php 
+    //set default value of variables for initial page load
+    if (!isset($firstName)) { $firstName = ''; } 
+    if (!isset($lastName)) { $lastName = ''; } 
+    if (!isset($username)) { $username = ''; } 
+    if (!isset($email)) { $email = ''; }
+?> 
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <title>CHANGE ME</title>
-    </head>
-    <body>
-			<h1>Hello</h1>
-    </body>
+<head>
+    <title>Registration Page</title>
+    <link rel="stylesheet" type="text/css" href="main.css">
+</head>
+
+<body>
+    <main>
+    <h1>Registration Page</h1>
+    <?php if (!empty($error_message)) { ?>
+        <p class="error"><?php echo htmlspecialchars($error_message); ?></p>
+    <?php } ?>
+    <form action="confirmation.php" method="post">
+
+        <div id="data">
+            <label>First Name:</label>
+            <input type="text" name="firstName"
+                   value="<?php echo htmlspecialchars($firstName); ?>">
+            <br>
+
+            <label>Last Name:</label>
+            <input type="text" name="lastName"
+                   value="<?php echo htmlspecialchars($lastName); ?>">
+            <br>
+
+            <label>User Name:</label>
+            <input type="text" name="userName"
+                   value="<?php echo htmlspecialchars($userName); ?>">
+            <br>
+            
+            <label>Email Address:</label>
+            <input type="text" name="email"
+                   value="<?php echo htmlspecialchars($email); ?>">
+            <br>
+        </div>
+
+        <div id="buttons">
+            <label>&nbsp;</label>
+            <input type="submit" value="Calculate"><br>
+        </div>
+
+    </form>
+    </main>
+</body>
 </html>
